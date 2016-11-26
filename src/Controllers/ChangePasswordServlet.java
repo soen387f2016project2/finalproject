0d4ed8a29c86d0ceed22f8e7860b15ba3dec0a71
DESKTOP-FFFAK0D\tino_ <tino_mvi@hotmail.com>
2016-11-25 23:22:54 -0500

    Adding Samatha Update Password code to my branch

--- src/Controllers/ChangePasswordServlet.java
+++ src/Controllers/ChangePasswordServlet.java
@@ -5,27 +5,24 @@
  */
 package Controllers;
 
-import DAO.UsersDAO;
-import Demo.UsersWeb;
-
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import javax.servlet.RequestDispatcher;
-
+import javax.servlet.http.HttpSession;
+import DAO.UsersDAO;
+import Demo.UsersWeb;
 
 /**
  *
- * @author mimi
+ * @author Samantha Graham
  */
-
 @WebServlet(name = "ChangePasswordServlet", urlPatterns = {"/ChangePasswordServlet"})
 public class ChangePasswordServlet extends HttpServlet {
+
     /**
      * Handles the HTTP <code>POST</code> method.
      *
@@ -36,20 +33,54 @@ public class ChangePasswordServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Get the data from the request
+        String currentPw = request.getParameter("currentPassword");
+        String newPw = request.getParameter("newPassword");
+        String confirmPw = request.getParameter("newPasswordConfirm");
         
-        // Get the input
-        HttpSession session = request.getSession();
-        String newPassword = request.getParameter("newPassword");      
-        int userId = Integer.getInteger(session.getAttribute("user_id").toString());
+        // Prepare to return
+        RequestDispatcher rd = request.getRequestDispatcher("change-password.jsp");
         
-        // Add checks...
+        // First, make sure nothing is empty
+        if(currentPw.isEmpty() || newPw.isEmpty() || confirmPw.isEmpty()) {            
+            // Give them a message
+            request.setAttribute("error", true);
+            request.setAttribute("message", "You must fill in all fields");
+        } else {
+            // Next, make sure the password and its confirmation are equal
+            if(!newPw.equals(confirmPw)) {
+                // Give them a message
+                request.setAttribute("error", true);
+                request.setAttribute("message", "Passwords don't match");
+            } else {
+                // Now let's see if they entered the right current password! So exciting.
+                // Instantiate the UsersDAO
+                UsersDAO dao = new UsersDAO();
+                
+                // Get the user's id from the session
+                HttpSession session = request.getSession();
+                String userId = session.getAttribute("user_id").toString();
+                
+                // User it to get their password from the db
+                String userPassword = dao.getPassword(userId);
+                
+                // Check equality
+                if(!currentPw.equals(userPassword)) {
+                    // Give them a message
+                    request.setAttribute("error", true);
+                    request.setAttribute("message", "You entered an incorrect current password");
+                } else {
+                    // Actually update the damn password
+                    dao.updatePassword(Integer.parseInt(userId), newPw);
+                    
+                    // And let them know!
+                    request.setAttribute("error", false);
+                    request.setAttribute("message", "Password successfully updated!");
+                }
+            }
+        }
         
-        // Assume that the password is valid and update in the DB
-        UsersDAO dao = new UsersDAO();
-        dao.updatePassword(userId, newPassword);
-      
-        // Return to the dashboard
-        RequestDispatcher rd = request.getRequestDispatcher("dashboard.jsp");    
+        // Return
         rd.forward(request, response);
     }
 }

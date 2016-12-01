8e43c672a2804c8bff033de6264d6b82c271eb2a
Adrian-Gabriel <Adrian_gabriel_1@hotmail.com>
2016-12-01 02:31:20 -0500

    Add files via upload

--- UpdateServlet.java
+++ UpdateServlet.java
@@ -26,33 +26,41 @@ public class UpdateServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
   
+          
+        String Update = request.getParameter("updateResource");
+        String Delete = request.getParameter("delete");
+        String Add = request.getParameter("AddResource");
+        
+        if(Update!= null && Delete==null && Add == null )
+        {
+             GetByID(request);
+        }
+         if (Delete!=null && Update ==null && Add == null)
+        {
+          DeleteByID(request);
+        }
+        if (Delete==null && Update ==null && Add != null)
+        {
+          AddByID(request);
+        }
+       
+        RequestDispatcher rd = request.getRequestDispatcher("update-resource.jsp");  
+        rd.forward(request, response);
+    }
+    
+    public void GetByID(HttpServletRequest request)
+    {
+       // Create the Resource Object
+        ResourcesDAO e = new ResourcesDAO();
         
         int ID = Integer.parseInt(request.getParameter("resourceId"));
         int ComputerType = Integer.parseInt(request.getParameter("computerType"));   
         String ComputerModel = request.getParameter("computerModel");
-        
         float ScreenSize = Float.parseFloat(request.getParameter("computerSize"));   
         String CPU = request.getParameter("computerCPU");
         String RAM = request.getParameter("computerRAM");  
         String Storage = request.getParameter("computerStorage"); 
         
-        if(request.getParameter("updateResource")=="update")
-        {
-             GetByID(ID,ComputerType,ComputerModel,ScreenSize,CPU,RAM,Storage);
-        }
-        else if (request.getParameter("updateResource")=="DELETE")
-        {
-            DeleteByID(ID);
-        }
-        RequestDispatcher rd = request.getRequestDispatcher("update-resource.jsp");  
-        rd.forward(request, response);
-    }
-    
-    private void GetByID(int ID,int ComputerType,String ComputerModel,Float ScreenSize,String CPU,String RAM,String Storage)
-    {
-       // Create the Resource Object
-        ResourcesDAO e = new ResourcesDAO();
-                       
         // Update the ID
         e.updateComputerType(ID, ComputerType);
         e.updateComputerModel(ID,ComputerModel);
@@ -64,13 +72,34 @@ public class UpdateServlet extends HttpServlet {
        
     }
     
-    private void DeleteByID(int ID)
+    private void DeleteByID(HttpServletRequest request)
     {
        // Create the Resource Object
         ResourcesDAO e = new ResourcesDAO();
-                       
+        
+        int ID = Integer.parseInt(request.getParameter("resourceId"));
+        
+        
         // Update the ID
        e.deleteResourceById(ID);
+       
+    }
+    
+     private void AddByID(HttpServletRequest request)
+    {
+       // Create the Resource Object
+        ResourcesDAO e = new ResourcesDAO();
+        
+        int ID = Integer.parseInt(request.getParameter("resourceId"));
+        int ComputerType = Integer.parseInt(request.getParameter("computerType"));   
+        String ComputerModel = request.getParameter("computerModel");
+        float ScreenSize = Float.parseFloat(request.getParameter("computerSize"));   
+        String CPU = request.getParameter("computerCPU");
+        String RAM = request.getParameter("computerRAM");  
+        String Storage = request.getParameter("computerStorage"); 
+        
+        // Update the ID
+       e.addResourceComputer(0, "Name"+ID ,ComputerType ,ComputerModel,ScreenSize,CPU,RAM, Storage);
        
     }
     
